f8e2094064d7fa99e71d7479ec92d3e2c9f6cb64
Reuter Junior <reuter_junior@hotmail.com>
2020-12-06 23:27:35 -0300

    inicializados para o mapa e marcadores no navegador

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 // import { User } from './User'
-// import { Company } from './Company'
+import { Company } from './Company'
 import { CustomMap } from './CustomMap'
 import { User } from './User'
 
 const user = new User()
+const company = new Company()
 const map = new CustomMap('map')
 
-map.addUserMarker(user)
+map.addMarker(user)
+map.addMarker(company)
